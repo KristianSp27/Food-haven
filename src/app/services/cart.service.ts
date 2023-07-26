25eb95599ae92a7235e23010019f621ac70a1549
KristianSp27 <kristian.spasov27@gmail.com>
2023-07-26 22:29:42 +0300

    Update

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -45,8 +45,14 @@ private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart);
     .reduce((prevSum, currentItem) => prevSum + currentItem.price, 0);
     this.cart.totalCount = this.cart.items
     .reduce((prevSum, currentItem) => prevSum + currentItem.quantity, 0)
-    
+
     const cartJson = JSON.stringify(this.cart);
-    localStorage.setItem('Cart', cartJson)
+    localStorage.setItem('Cart', cartJson);
+    this.cartSubject.next(this.cart);
+  }
+
+  private getCartFromLocalStorage():Cart{
+    const cartJson = localStorage.getItem('Cart');
+    return cartJson? JSON.parse(cartJson): new Cart();
   }
 }

51b4c188258a343a7bf875e607ed03c4243e9ab6
KristianSp27 <kristian.spasov27@gmail.com>
2023-07-27 23:24:17 +0300

    Update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/partials/header/header.component';
+import { HeaderComponent } from './componens/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { StarRatingComponent } from './components/partials/star-rating/star-rating.component';
 import { SearchComponent } from './components/partials/search/search.component';

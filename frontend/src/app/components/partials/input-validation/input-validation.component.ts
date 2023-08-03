4058a2cac71372bf2040aa9112a7d94b1713a2c7
Kristian Spasov <kristian.spasov27@gmail.com>
2023-08-03 18:40:19 +0300

    Update

--- frontend/src/app/components/partials/input-validation/input-validation.component.ts
+++ frontend/src/app/components/partials/input-validation/input-validation.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges, ɵɵNgOnChangesFeature } from '@angular/core';
-import { AbstractControl, Validators } from '@angular/forms';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
 
 const VALIDATORS_MESSAGES:any = {
-  required: 'Should not be empty',
-  email: 'Email is not valid'
+  required:'Should not be empty',
+  email:'Email is not valid',
+  minlength: 'Field is too short',
+  notMatch: 'Password and Confirm does not match'
 }
 
 @Component({
@@ -11,35 +13,37 @@ const VALIDATORS_MESSAGES:any = {
   templateUrl: './input-validation.component.html',
   styleUrls: ['./input-validation.component.css']
 })
-export class InputValidationComponent implements OnInit, OnChanges {
-@Input()
-control!:AbstractControl;
-@Input()
-showErrorsWhen:boolean = true;
-errorMessages: string[] = [];
-constructor() { }
+export class InputValidationComponent implements OnInit,OnChanges {
+  @Input()
+  control!:AbstractControl;
+  @Input()
+  showErrorsWhen:boolean = true;
+  errorMessages: string[] = [];
+  constructor() { }
 
-ngOnChanges(changes: SimpleChanges): void {
-  this.checkValidation();
-}
-
-ngOnInit(): void {
-this.control.statusChanges.subscribe(() => {
-  this.checkValidation();
-});
-this.control.valueChanges.subscribe(() => {
-  this.checkValidation();
-})
-}
-
-checkValidation(){
-  const errors = this.control.errors;
-  if(!errors){
-    this.errorMessages = [];
-    return;
+  ngOnChanges(changes: SimpleChanges): void {
+    this.checkValidation();
   }
 
-  const errorKeys = Object.keys(errors);
-  this.errorMessages = errorKeys.map(key => VALIDATORS_MESSAGES[key]);
-}
+  ngOnInit(): void {
+    this.control.statusChanges.subscribe(() => {
+      this.checkValidation();
+    });
+    this.control.valueChanges.subscribe(() => {
+      this.checkValidation();
+    })
+  }
+
+  checkValidation(){
+    const errors = this.control.errors;
+    if(!errors){
+      this.errorMessages = [];
+      return;
+    }
+
+    const errorKeys = Object.keys(errors);
+    this.errorMessages = errorKeys.map(key => VALIDATORS_MESSAGES[key]);
+
+  }
+
 }

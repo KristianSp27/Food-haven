be304a7f75040b97130f4c7f0891caa78775e6d3
Kristian Spasov <kristian.spasov27@gmail.com>
2023-08-09 23:30:39 +0300

    Update

--- frontend/src/app/components/pages/register-page/register-page.component.ts
+++ frontend/src/app/components/pages/register-page/register-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
@@ -10,32 +10,31 @@ import { PasswordsMatchValidator } from 'src/app/shared/validators/password_matc
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit {
 
   registerForm!:FormGroup;
   isSubmitted = false;
 
   returnUrl = '';
-
   constructor(
     private formBuilder: FormBuilder,
     private userService: UserService,
     private activatedRoute: ActivatedRoute,
     private router: Router
-  ) {}
+  ) { }
 
-  ngonInit(): void {
+  ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(5)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(5)]],
       confirmPassword: ['', Validators.required],
       address: ['', [Validators.required, Validators.minLength(10)]]
-    }, {
-      validators: PasswordsMatchValidator('password', 'confirmPassword')
+    },{
+      validators: PasswordsMatchValidator('password','confirmPassword')
     });
 
-    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl;
+    this.returnUrl= this.activatedRoute.snapshot.queryParams.returnUrl;
   }
 
   get fc() {
@@ -46,7 +45,7 @@ export class RegisterPageComponent {
     this.isSubmitted = true;
     if(this.registerForm.invalid) return;
 
-    const fv = this.registerForm.value;
+    const fv= this.registerForm.value;
     const user :IUserRegister = {
       name: fv.name,
       email: fv.email,
@@ -59,4 +58,5 @@ export class RegisterPageComponent {
       this.router.navigateByUrl(this.returnUrl);
     })
   }
+
 }

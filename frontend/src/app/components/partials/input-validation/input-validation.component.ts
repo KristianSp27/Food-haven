feb5ae9af94371534a104d23d1e4d3114f73fb8e
Kristian Spasov <kristian.spasov27@gmail.com>
2023-08-09 23:45:47 +0300

    Update

--- frontend/src/app/components/partials/input-validation/input-validation.component.ts
+++ frontend/src/app/components/partials/input-validation/input-validation.component.ts
@@ -4,8 +4,8 @@ import { AbstractControl } from '@angular/forms';
 const VALIDATORS_MESSAGES:any = {
   required:'Should not be empty',
   email:'Email is not valid',
-  minlength: 'Field is too short',
-  notMatch: 'Password and Confirm does not match'
+  minlength: 'Name is too short',
+  notMatch: 'Passwords do not match'
 }
 
 @Component({

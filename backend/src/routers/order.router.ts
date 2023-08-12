fe7c5a65a74c10dfd60c7ab03aa4897dc9f49766
Kristian Spasov <kristian.spasov27@gmail.com>
2023-08-12 11:24:35 +0300

    Update

--- backend/src/routers/order.router.ts
+++ backend/src/routers/order.router.ts
@@ -1,6 +1,24 @@
 import { Router } from "express";
 import asyncHandler from "express-async-handler";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+import { OrderModel } from "../models/order.model";
+import { OrderStatus } from "../constants/order_status";
+import auth from '../middlewares/auth.mid';
 
 const router = Router();
+router.use(auth);
 
-router.post('/create',)
+router.post('/create',
+asyncHandler(async (req:any, res:any) => {
+    const requestOrder = req.body;
+
+    if(requestOrder.items.length <= 0) {
+        res.status(HTTP_BAD_REQUEST).send('Your cart is empty!');
+        return;
+    }
+
+    await OrderModel.deleteOne({
+        user: req.user.id,
+        status: OrderStatus.NEW
+    })
+}))
